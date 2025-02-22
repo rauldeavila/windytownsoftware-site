854a72f0acea0594e1e786fecb331ab29b78ef6e
Raul (bwulf) <rauldeavila93@gmail.com>
2025-02-22 08:34:05 -0300

    fix apple app site association

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === '/.well-known/apple-app-site-association') {
+    return new NextResponse(JSON.stringify({
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: "5L5C42S46R.name.rajunior.TodoList",
+            paths: ["/share*"]
+          }
+        ]
+      }
+    }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+}
+
+export const config = {
+  matcher: '/.well-known/apple-app-site-association'
+}
